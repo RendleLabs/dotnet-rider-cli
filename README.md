4d4f8550cc917fd97a9304ebc0d7e268ab015787
Mark Rendle <mark@rendle.io>
2018-08-05 20:52:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # dotnet-rider-cli
-.NET Global tool to launch JetBrains Rider from the CLI on Windows
+
+This is a simple .NET Global tool to launch JetBrains Rider from the CLI on Windows.
+
+## Installation
+
+Windows-only for now:
+
+```shell
+λ dotnet tool install -g dotnet-rider-cli
+```
+
+## Usage
+
+To open the current directory:
+```shell
+λ rider .
+```
+To open a solution:
+```shell
+λ rider Thing.sln
+```
+
+## Why?
+
+I install Rider using the JetBrains toolbox, and I generally
+use the Nightly builds, so the install location changes
+a lot. I don't want to keep updating an alias, so I made this.
+It finds all the installations of Rider under
+`%LOCALAPPDATA%\JetBrains\Toolbox\apps\Rider` and runs
+the most recent version it finds.
+
+Hope somebody else finds it useful.
---
+++ src/RiderLauncher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RiderLauncher
+{
+    class Program
+    {
+        static readonly Regex VersionNumberExtractor = new Regex(@"[0-9]+\.[0-9]+\.[0-9]+");
+        static void Main(string[] args)
+        {
+            var folderPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                @"JetBrains\Toolbox\apps\Rider"
+            );
+            
+            if (Directory.Exists(folderPath))
+            {
+                var latest = Directory.EnumerateFiles(folderPath, "rider64.exe", SearchOption.AllDirectories)
+                    .OrderByDescending(f => VersionNumberExtractor.Match(f).Value)
+                    .FirstOrDefault();
+
+                if (latest != null)
+                {
+                    var arguments = string.Join(" ", args.Select(a => a.Contains(' ') ? $"\"{a}\"" : a));
+                    Console.WriteLine($"Starting: {latest} {arguments}");
+                    var psi = new ProcessStartInfo
+                    {
+                        UseShellExecute = true,
+                        FileName = latest,
+                        Arguments = arguments
+                    };
+                    Process.Start(psi);
+                }
+            }
+        }
+    }
+}
